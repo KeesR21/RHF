60192d7c8356b2e1b557f74b76b14f000ae12db0
MURENZI Bienvenu <bmurenzi25@gmail.com>
2021-08-08 10:45:19 +0200

    correced longText error

--- database/migrations/2021_07_27_130104_create_posts_table.php
+++ database/migrations/2021_07_27_130104_create_posts_table.php
@@ -17,7 +17,7 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->text('content');
+            $table->longText('content');
             $table->string('image');
             $table->timestamps();
         });
